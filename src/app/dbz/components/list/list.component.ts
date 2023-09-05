1ea49d7c2acb732e510ac62a4f05614fa541a6a8
Amparo Guillén Requena <ampguireq@gmail.com>
2023-09-05 18:20:52 +0100

    Creamos el servicio privado dbz

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -16,11 +16,12 @@ export class ListComponent {
     }];
 
     @Output()
-    public onDelete: EventEmitter<number> = new EventEmitter();
+    public onDelete: EventEmitter<string> = new EventEmitter();
 
-    public onDeleteCharacter(index: number): void {
-      //TODO: Emitir el ID del Personaje
+    public onDeleteCharacter( id?: string): void {
 
-      this.onDelete.emit(index);
+      if ( !id ) return;
+      this.onDelete.emit( id );
+
     }
 }
